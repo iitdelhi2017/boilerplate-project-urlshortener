053656fbe520cb56b453bfcab315ba248de2b9aa
Amulya <iitdelhi2017@gmail.com>
2021-05-18 16:53:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+var mongoose=require('mongoose');
+var bodyParser=require('body-parser');
+var dns=require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,3 +25,46 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+//-------------------------------------------------------------------------
+
+
+mongoose.connect("mongodb+srv://user2:user2@cluster0.tcl4l.mongodb.net/myDatabase?retryWrites=true&w=majority"
+, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+var urlSchema=new mongoose.Schema({
+  url:String,
+  shorturl:{
+    type:Number,
+    default:function() {
+      return Math.floor(Math.random()*10000000000+Math.random()*10000000000); 
+    }
+  }
+});
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+var urls=mongoose.model('URL',urlSchema);
+
+var create_url=function(req,res){
+  var URL=new urls({url:req.body.url});
+  URL.save(function(err,data){
+    if(err) console.error(err);
+    res.json({"original_url":data.url,"short_url":data.shorturl});
+  });
+};
+
+var use_short=function(req,res){
+  urls.findOne({shorturl:parseInt(req.params.code)},function(err,data){
+    if(err) console.error(err);
+    dns.lookup(data.url,function(err,data2){
+      if(err) res.json({"error":"invalid url"});
+      res.redirect(data.url);
+    });
+  });
+};
+
+app.post('/api/shorturl',create_url);
+app.get('/api/shorturl/:code',use_short);
+//-----------------------------------------------------------------------------
